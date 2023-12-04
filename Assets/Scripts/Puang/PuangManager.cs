fc07807db5dabb269e9c58f68555ce354e866e14
Benzene Kim <davidkim020409@gmail.com>
2023-12-04 23:25:38 +0900

    feature: add jump and double jump function

--- Assets/Scripts/Puang/PuangManager.cs
+++ Assets/Scripts/Puang/PuangManager.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum PuangState
+{
+    IDLE,
+    RUNNING,
+    JUMP,
+    DOUBLE_JUMP,
+    DIE,
+}
+
 public class PuangManager : MonoBehaviour
 {
     public Rigidbody2D rb;
+    private PuangState _state = PuangState.IDLE;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +29,34 @@ public class PuangManager : MonoBehaviour
 
     private IEnumerator InputManager()
     {
+        while (_state == PuangState.IDLE) yield return new WaitForSeconds(0.01f);
         while (true)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,5), ForceMode2D.Impulse);
+                switch (_state)
+                {
+                    case PuangState.JUMP:
+                        _state = PuangState.DOUBLE_JUMP;
+                        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                        break;
+                    case PuangState.RUNNING:
+                        _state = PuangState.JUMP;
+                        this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                        break;
+                }
             }
             yield return new WaitForSeconds(float.MinValue);
+            
         }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform") _state = PuangState.RUNNING;
+        else if (collision.gameObject.tag == "Obstacle") _state = PuangState.DIE;
+
     }
 }
