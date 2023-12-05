3646fe68c0f773f3ca20808fdb80cc0ef138b856
Marienini <149225439+Marienini@users.noreply.github.com>
2023-12-05 22:13:48 +0900

    feature : room with puang

---
+++ Assets/Scripts/Puang/PuangMainScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuangMainScene : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Le sprite a été cliqué !");
+    }
+}
